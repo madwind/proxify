d3c4fb3836d164b5c2068745ee7a56c6563f1c59
IvonWei <madwind.cn@gmail.com>
2026-09-09 10:07:51 +0800

    fix: enable automatic upstream HTTP negotiation

--- src/proxy.rs
+++ src/proxy.rs
@@ -51,7 +51,6 @@ impl AppState {
         let client = Client::builder()
             .timeout(Duration::from_secs(60))
             .no_proxy()
-            .http1_only()
             .pool_idle_timeout(None)
             .pool_max_idle_per_host(10)
             .tls_danger_accept_invalid_certs(true)
